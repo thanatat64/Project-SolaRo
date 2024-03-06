0595af45236b1c555454ef8a8e40aff362b69682
thanatat64 <thanatat64@gmail.com>
2024-03-07 03:28:34 +0700

    nativewind

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:@react-native/babel-preset',"@babel/preset-env", "@babel/preset-react",'nativewind/babel'],
+  plugins: ["nativewind/babel"],
 };
